95785ca8b140c679ce1de36885ec1607b3e3682a
Holfe <wvcneto@gmail.com>
2019-10-13 21:47:52 -0300

    Test Flex Layout

--- index.js
+++ index.js
@@ -2,11 +2,42 @@
 import React from 'react';
 import {AppRegistry, View, Text} from 'react-native';
 
+//Format
+
+const Style = {
+  main: {
+    backgroundColor: '#ccc',
+    flex: 1, // 1 de 1:1
+  },
+  header: {
+    backgroundColor: '#f1a',
+    flex: 2, // de 2:12
+  },
+  content: {
+    backgroundColor: '#b1a',
+    flex: 9, // de 9:12
+  },
+  footer: {
+    backgroundColor: '#71a',
+    flex: 1, // de 1:12
+  },
+  viewStyle: {
+    backgroundColor: '#fff',
+    height: 400,
+    justifyContent: 'space-around',
+    alignItems: 'stretch',
+    flexDirection: 'column',
+  },
+};
+
 //Components
 const App = () => {
+  const {main, header, content, footer} = Style; //Destructuring Assingment
   return (
-    <View>
-      <Text>Frases do Dia!</Text>
+    <View style={main}>
+      <Text style={header}>Header</Text>
+      <Text style={content}>Content</Text>
+      <Text style={footer}>Footer</Text>
     </View>
   );
 };
